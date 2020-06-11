483f2ba3aa55980e15a018cbe804acc396f1c9ee
ДМ. Па <qtckpuhdsa@gmail.com>
2020-06-11 14:17:41 +0300

    Create basic form for running scripts

---
+++ Svd_to_animation/Svd_to_animation/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Svd_to_animation
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(409, 159);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Папка, куда будут сохраняться результаты";
+            this.toolTip1.SetToolTip(this.groupBox1, "Если записать несуществующую папку, она будет создана перед началом конвертации");
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(3, 117);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(403, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Изменить (выбрать другую)";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox1.Location = new System.Drawing.Point(3, 22);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(403, 89);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.DodgerBlue;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Location = new System.Drawing.Point(99, 336);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(244, 53);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Запустить процесс";
+            this.toolTip1.SetToolTip(this.button3, "Не забудьте указать шаг по времени (после прочтения файла)");
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Location = new System.Drawing.Point(16, 210);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(405, 89);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Конвертируемый файл";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.OldLace;
+            this.button2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(318, 22);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 64);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Выбрать";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.textBox2.Location = new System.Drawing.Point(3, 22);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(309, 64);
+            this.textBox2.TabIndex = 0;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip1.Location = new System.Drawing.Point(0, 418);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(437, 25);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(86, 22);
+            this.toolStripLabel1.Text = "toolStripLabel1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(437, 443);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "svd to image series";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+    }
+}
+
---
+++ Svd_to_animation/Svd_to_animation/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using МатКлассы;
+using System.Diagnostics;
+
+namespace Svd_to_animation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitFolder();
+            button3.Hide();
+
+            toolStrip1.Hide();
+            toolStripLabel1.Text = "Выберите нужные директории";
+        }
+
+        public string storageDirectoryFile = "StorageDirectory.txt";
+        
+        public void InitFolder()
+        {
+
+            if (File.Exists(storageDirectoryFile))
+            {
+                var dir = Expendator.GetWordFromFile(storageDirectoryFile);
+
+                if (Directory.Exists(dir))
+                    textBox1.Text = dir;
+                else
+                {
+                    MessageBox.Show($"Директория {dir} из файла {storageDirectoryFile} не существует. Используется базовая директория", "Нет указанной директории", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    textBox1.Text = Environment.CurrentDirectory;
+                }
+            }
+            else
+            {
+                textBox1.Text = Environment.CurrentDirectory;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderDlg = new FolderBrowserDialog())
+            {
+                folderDlg.ShowNewFolderButton = true;
+                DialogResult result = folderDlg.ShowDialog();
+
+                if (result == DialogResult.OK)
+                {
+                    textBox1.Text = folderDlg.SelectedPath;
+                    Expendator.WriteStringInFile(Path.Combine(Environment.CurrentDirectory, storageDirectoryFile), textBox1.Text);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string init = "c:\\";
+            if(textBox2.Text != "")
+            {
+                init = Path.GetDirectoryName(textBox2.Text);
+            }
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = init;
+                openFileDialog.Filter = "svd files (*.svd)|*.svd";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    textBox2.Text = openFileDialog.FileName;
+                    button3.Show();
+                }
+            }
+
+            toolStripLabel1.Text = "Теперь можно запускать процесс";
+        }
+
+
+
+        private void CopyResourses(string to)
+        {
+            string[] pyfiles = { "create.py", "heatmap.py", "get_data_from_file.py" };
+
+            foreach (var f in pyfiles)
+                File.Copy(f, Path.Combine(to, f), true);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(textBox1.Text))
+            {
+                try
+                {
+                    Directory.CreateDirectory(textBox1.Text);
+                }
+                catch
+                {
+                    MessageBox.Show("Не получилось создать указанную папку! Выберите другую", "Ошибка с папкой", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                Expendator.WriteStringInFile(Path.Combine(Environment.CurrentDirectory, storageDirectoryFile), textBox1.Text);
+            }
+
+            string newFolder = Path.Combine(textBox1.Text, Path.GetFileNameWithoutExtension(textBox2.Text));
+            if(!Directory.Exists(newFolder))
+                Directory.CreateDirectory(newFolder);
+            CopyResourses(newFolder);
+
+            Expendator.WriteStringInFile(Path.Combine(newFolder, "path.txt"), textBox2.Text);
+
+
+            this.Hide();
+
+            StartProcess(Path.Combine(newFolder, "create.py"), () => {
+
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(Properties.Resources.end);
+                player.Play();
+                this.Show();
+
+            });
+        }
+         
+        private void StartProcess(string fileName, Action act)
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = fileName;
+            process.EnableRaisingEvents = true;
+            process.StartInfo.UseShellExecute = true;
+            process.StartInfo.Arguments = "py.exe";
+
+            process.Exited += (sender, e) => act();
+            process.Start();
+
+            //Process.Start(@"cmd.exe ", @$"py.exe {fileName}");
+
+            process.WaitForExit();
+
+        }
+
+
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
